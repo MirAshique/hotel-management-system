8db1b31b3e3008d671430a62be8fbec5ccf56b33
MirAshique <ashiqtalpur18.com>
2026-02-10 20:42:59 +0500

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://hotel-backend-dsii.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
